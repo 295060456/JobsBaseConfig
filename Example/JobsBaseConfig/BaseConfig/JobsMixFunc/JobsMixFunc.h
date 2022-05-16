1535be18d1a7de4fa796e8074f3883f60682352f
Jobs <lg295060456@vip.qq.com>
2022-05-16 17:15:55 +0700

    。
    。
    
    Signed-off-by: Jobs <lg295060456@vip.qq.com>

--- Example/JobsBaseConfig/BaseConfig/JobsMixFunc/JobsMixFunc.h
+++ Example/JobsBaseConfig/BaseConfig/JobsMixFunc/JobsMixFunc.h
@@ -14,6 +14,7 @@
 #import "JobsHotLabelWithSingleLine.h"
 #import "JobsHotLabelWithMultiLine.h"
 #import "JobsPageView.h"
+#import "JobsTitleView01.h"
 #import "JobsBasePopupView.h"
 #import "JobsBitsMonitor.h"
 #import "JobsSuspend.h"
