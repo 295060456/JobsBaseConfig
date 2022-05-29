51ce1ab46d195b0c5fbb76e49e9708ac3bbc237c
Jobs <jobs@JobsdeMac-mini.local>
2022-05-29 11:27:53 +0700

    .
    .

--- Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UITableViewCell/UITableViewCell+Category/UITableViewCell+Margin/UITableViewCell+Margin.h
+++ Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UITableViewCell/UITableViewCell+Category/UITableViewCell+Margin/UITableViewCell+Margin.h
@@ -24,12 +24,12 @@ BaseCellProtocol
 /// @param dx 内有介绍
 /// @param dy 内有介绍
 /// @param indexPath indexPath
-+(void)tableView:(UITableView *)tableView
-makeFirstAndLastCell:(nonnull UITableViewCell *)cell
++(void)tableView:(UITableView *_Nonnull)tableView
+makeFirstAndLastCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
      roundCorner:(CGFloat)radius
               dx:(CGFloat)dx
-              dy:(CGFloat)dy
-     atIndexPath:(nonnull NSIndexPath *)indexPath;
+              dy:(CGFloat)dy;
 /// 以section为单位，每个section的第一行和最后一行的cell圆角化处理
 /// @param tableView 作用对象tableView
 /// @param cell 作用对象Tableviewcell
@@ -37,12 +37,22 @@ makeFirstAndLastCell:(nonnull UITableViewCell *)cell
 /// @param dx 内有介绍
 /// @param dy 内有介绍
 /// @param indexPath indexPath
-+(void)tableView:(UITableView *)tableView
-makeSectionFirstAndLastCell:(nonnull UITableViewCell *)cell
++(void)tableView:(UITableView *_Nonnull)tableView
+makeSectionFirstAndLastCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
+       cellBgCor:(UIColor *_Nullable)cellBgCor
+   bottomLineCor:(UIColor *_Nullable)bottomLineCor
+  cellOutLineCor:(UIColor *_Nullable)cellOutLineCor
      roundCorner:(CGFloat)radius
               dx:(CGFloat)dx
-              dy:(CGFloat)dy
-     atIndexPath:(nonnull NSIndexPath *)indexPath;
+              dy:(CGFloat)dy;
+/// 除了最后一行以外，所有的cell的最下面的线的颜色为bottomLineCor
++(void)tableView:(UITableView *_Nonnull)tableView
+ makeLastRowCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
+   bottomLineCor:(UIColor *_Nullable)bottomLineCor
+              dx:(CGFloat)dx
+              dy:(CGFloat)dy;
 
 @end
 
@@ -57,19 +67,22 @@ NS_ASSUME_NONNULL_END
      ///  UITableViewCell 第一行和最后一行圆角设置
      [UITableViewCell tableView:tableView
            makeFirstAndLastCell:cell
-                    roundCorner:6
-                             dx:JobsWidth(16)
-                             dy:0
-                    atIndexPath:indexPath];
+                    atIndexPath:indexPath
+                    roundCorner:JobsWidth(8)
+                             dx:JobsWidth(1)
+                             dy:0];
  
  或者是
      /// 以section为单位，每个section的第一行和最后一行的cell圆角化处理
      [UITableViewCell tableView:tableView
     makeSectionFirstAndLastCell:cell
+                    atIndexPath:indexPath
+                      cellBgCor:UIColor.whiteColor
+                  bottomLineCor:UIColor.whiteColor
+                 cellOutLineCor:HEXCOLOR(0xEEE2C8)
                     roundCorner:JobsWidth(8)
                              dx:JobsWidth(1)
-                             dy:0
-                    atIndexPath:indexPath];
+                             dy:0];
  }
  **/
 
--- Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UITableViewCell/UITableViewCell+Category/UITableViewCell+Margin/UITableViewCell+Margin.m
+++ Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UITableViewCell/UITableViewCell+Category/UITableViewCell+Margin/UITableViewCell+Margin.m
@@ -24,24 +24,6 @@
 //}
 
 #pragma mark —— 功能方法
-+(CGRect)tableViewCell:(nonnull UITableViewCell *)tableViewCell
-                    dx:(CGFloat)dx
-                    dy:(CGFloat)dy{
-    
-    /// CGRectInset会进行平移和缩放两个操作;CGRectOffset做的只是平移
-    /**
-     CGRectInset(CGRect rect, CGFloat dx, CGFloat dy),
-     三个参数:
-     rect：待操作的CGRect；
-     dx：为正数时，向右平移dx，宽度缩小2dx。为负数时，向左平移dx，宽度增大2dx；
-     dy：为正数是，向下平移dy，高度缩小2dy。为负数是，向上平移dy，高度增大2dy。
-     
-     **/
-    // 获取显示区域大小
-    return CGRectInset(tableViewCell.bounds,
-                       dx,
-                       0);
-}
 /// iOS UITableViewCell 第一行和最后一行圆角设置
 /// @param tableView 作用对象tableView
 /// @param cell 作用对象Tableviewcell
@@ -49,21 +31,22 @@
 /// @param dx 内有介绍
 /// @param dy 内有介绍
 /// @param indexPath indexPath
-+(void)tableView:(UITableView *)tableView
-makeFirstAndLastCell:(nonnull UITableViewCell *)cell
++(void)tableView:(UITableView *_Nonnull)tableView
+makeFirstAndLastCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
      roundCorner:(CGFloat)radius
               dx:(CGFloat)dx
-              dy:(CGFloat)dy
-     atIndexPath:(nonnull NSIndexPath *)indexPath{
+              dy:(CGFloat)dy{
+    if (!tableView) return;
+    if (!cell) return;
+    if (!indexPath) return;
     // 圆角角度
     if (radius == 0) {
         radius = JobsWidth(10.f);
     }
-    
     if (dx == 0) {
         dx = JobsWidth(15);
     }
-    
     // 设置cell 背景色为透明
     cell.backgroundColor = UIColor.clearColor;
     // 创建两个layer
@@ -132,27 +115,45 @@ makeFirstAndLastCell:(nonnull UITableViewCell *)cell
 /// @param dx 内有介绍
 /// @param dy 内有介绍
 /// @param indexPath indexPath
-+(void)tableView:(UITableView *)tableView
-makeSectionFirstAndLastCell:(nonnull UITableViewCell *)cell
++(void)tableView:(UITableView *_Nonnull)tableView
+makeSectionFirstAndLastCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
+       cellBgCor:(UIColor *_Nullable)cellBgCor
+   bottomLineCor:(UIColor *_Nullable)bottomLineCor
+  cellOutLineCor:(UIColor *_Nullable)cellOutLineCor
      roundCorner:(CGFloat)radius
               dx:(CGFloat)dx
-              dy:(CGFloat)dy
-     atIndexPath:(nonnull NSIndexPath *)indexPath{
+              dy:(CGFloat)dy{
+    if (!tableView) return;
+    if (!cell) return;
+    if (!indexPath) return;
     // 圆角角度
     if (radius == 0) {
         radius = JobsWidth(10.f);
     }
-    
+    // 水平偏移量
     if (dx == 0) {
         dx = JobsWidth(15);
+    }
+    // cell的底色
+    if (!cellBgCor) {
+        cellBgCor = UIColor.whiteColor;
+    }
+    // cell下线的颜色
+    if (!bottomLineCor) {
+        bottomLineCor = UIColor.whiteColor;
+    }
+    // cell外线的颜色
+    if (!cellOutLineCor) {
+        cellOutLineCor = UIColor.whiteColor;
     }
     // 大小
     CGRect bounds = [self tableViewCell:cell dx:dx dy:dy];
     // 行数
     NSInteger numberOfRows = [tableView numberOfRowsInSection:indexPath.section];
+    
     // 绘制曲线
     UIBezierPath *bezierPath = nil;
-    
     if (indexPath.row == 0 && numberOfRows == 1) {
         // 一个为一组时,四个角都为圆角
         bezierPath = [UIBezierPath bezierPathWithRoundedRect:bounds
@@ -174,22 +175,82 @@ makeSectionFirstAndLastCell:(nonnull UITableViewCell *)cell
     }
     // cell的背景色透明
     cell.backgroundColor = UIColor.clearColor;
-    // 新建一个图层
-    CAShapeLayer *layer = CAShapeLayer.layer;
-    // 图层边框路径
-    layer.path = bezierPath.CGPath;
-    // 图层填充色,也就是cell的底色
-    layer.fillColor = UIColor.whiteColor.CGColor;
-    // 图层边框线条颜色
-    /*
-     如果self.tableView.style = UITableViewStyleGrouped时,每一组的首尾都会有一根分割线,目前我还没找到去掉每组首尾分割线,保留cell分割线的办法。
-     所以这里取巧,用带颜色的图层边框替代分割线。
-     这里为了美观,最好设为和tableView的底色一致。
-     设为透明,好像不起作用。
-     */
-    layer.strokeColor = UIColor.whiteColor.CGColor;
-    //将图层添加到cell的图层中,并插到最底层
-    [cell.layer insertSublayer:layer atIndex:0];
+    {
+        // 新建一个图层
+        CAShapeLayer *layer = CAShapeLayer.layer;
+        // 图层边框路径
+        layer.path = bezierPath.CGPath;
+        // 图层填充色,也就是cell的底色
+        layer.fillColor = cellBgCor.CGColor;
+        // 图层边框线条颜色
+        /*
+         如果self.tableView.style = UITableViewStyleGrouped时,每一组的首尾都会有一根分割线,目前我还没找到去掉每组首尾分割线,保留cell分割线的办法。
+         所以这里取巧,用带颜色的图层边框替代分割线。
+         这里为了美观,最好设为和tableView的底色一致。
+         设为透明,好像不起作用。
+         */
+        layer.strokeColor = cellOutLineCor.CGColor;
+        //将图层添加到cell的图层中,并插到最底层
+        [cell.layer insertSublayer:layer atIndex:0];
+    }
+    /// 除了最后一行以外，所有的cell的最下面的线的颜色为bottomLineCor
+    [UITableViewCell tableView:tableView
+               makeLastRowCell:cell
+                   atIndexPath:indexPath
+                 bottomLineCor:bottomLineCor
+                            dx:dx
+                            dy:dy];
+}
+/// 除了最后一行以外，所有的cell的最下面的线的颜色为bottomLineCor
++(void)tableView:(UITableView *_Nonnull)tableView
+ makeLastRowCell:(UITableViewCell *_Nonnull)cell
+     atIndexPath:(NSIndexPath *_Nonnull)indexPath
+   bottomLineCor:(UIColor *_Nullable)bottomLineCor
+              dx:(CGFloat)dx
+              dy:(CGFloat)dy{
+    if (!tableView) return;
+    if (!cell) return;
+    if (!indexPath) return;
+    if (dx == 0) {
+        dx = JobsWidth(15);
+    }
+    // 大小
+    CGRect bounds = [self tableViewCell:cell dx:dx dy:dy];
+    // 行数
+    NSInteger numberOfRows = [tableView numberOfRowsInSection:indexPath.section];
+    /// 除了最后一行以外，所有的cell的最下面的线为白色
+    if (indexPath.row != numberOfRows - 1) {
+        UIBezierPath *linePath = UIBezierPath.bezierPath;
+        // 起点
+        [linePath moveToPoint:CGPointMake(bounds.origin.x, bounds.size.height)];
+        // 其他点
+        [linePath addLineToPoint:CGPointMake(bounds.origin.x + bounds.size.width, bounds.size.height)];
+        // 新建一个图层
+        CAShapeLayer *layer = CAShapeLayer.layer;
+        layer.path = linePath.CGPath;
+        layer.strokeColor = UIColor.whiteColor.CGColor;
+        //将图层添加到cell的图层中,并插到最底层
+        [cell.layer insertSublayer:layer atIndex:1];
+    }
+}
+
++(CGRect)tableViewCell:(nonnull UITableViewCell *)tableViewCell
+                    dx:(CGFloat)dx
+                    dy:(CGFloat)dy{
+    
+    /// CGRectInset会进行平移和缩放两个操作;CGRectOffset做的只是平移
+    /**
+     CGRectInset(CGRect rect, CGFloat dx, CGFloat dy),
+     三个参数:
+     rect：待操作的CGRect；
+     dx：为正数时，向右平移dx，宽度缩小2dx。为负数时，向左平移dx，宽度增大2dx；
+     dy：为正数是，向下平移dy，高度缩小2dy。为负数是，向上平移dy，高度增大2dy。
+     
+     **/
+    // 获取显示区域大小
+    return CGRectInset(tableViewCell.bounds,
+                       dx,
+                       0);
 }
 static char *UITableViewCell_Margin_indexPath = "UITableViewCell_Margin_indexPath";
 @dynamic indexPath;
