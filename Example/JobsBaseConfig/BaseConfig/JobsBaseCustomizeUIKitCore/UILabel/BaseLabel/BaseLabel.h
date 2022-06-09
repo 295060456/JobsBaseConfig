47930edc77896a98fd0aad4a08936fbd9812a67c
Jobs <jobs@JobsdeMac-mini.local>
2022-06-09 20:04:05 +0700

    .
    .

--- Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UILabel/BaseLabel/BaseLabel.h
+++ Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UILabel/BaseLabel/BaseLabel.h
@@ -8,13 +8,14 @@
 #import <UIKit/UIKit.h>
 #import "UIView+Extras.h"
 #import "UILabel+Extra.h"
+#import "BaseViewProtocol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 /**
  自定义UILabel 实现显示偏移量和内边距
  https://www.jianshu.com/p/541ef795a1f2
  */
-@interface BaseLabel : UILabel
+@interface BaseLabel : UILabel<BaseViewProtocol>
 
 @property(nonatomic,assign)UIEdgeInsets edgeInsets;
 @property(nonatomic,assign)CGFloat offsetY;
