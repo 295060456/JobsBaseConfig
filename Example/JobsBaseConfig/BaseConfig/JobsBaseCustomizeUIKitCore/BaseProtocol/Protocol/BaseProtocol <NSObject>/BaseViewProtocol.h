8edcfe2c2893408273a4d750a59553f32bd8b478
jobs <jobs@gmail.com>
2023-09-20 03:13:17 +0700

    no message

--- Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/BaseProtocol/Protocol/BaseProtocol <NSObject>/BaseViewProtocol.h
+++ Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/BaseProtocol/Protocol/BaseProtocol <NSObject>/BaseViewProtocol.h
@@ -7,6 +7,13 @@
 
 #import <Foundation/Foundation.h>
 #import "BaseProtocol.h"
+
+#if __has_include(<Masonry/Masonry.h>)
+#import <Masonry/Masonry.h>
+#else
+#import "Masonry.h"
+#endif
+
 @class UIViewModel;
 
 typedef enum : NSInteger {
