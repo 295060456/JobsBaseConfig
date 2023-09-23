5d3f6e7751fd7520c92b8c6207184bac230b6976
jobs <jobs@gmail.com>
2023-09-24 05:45:07 +0700

    no message

--- Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UIButton/UIButton+Category/UIButton+UI/UIButton+UI.h
+++ Example/JobsBaseConfig/BaseConfig/JobsBaseCustomizeUIKitCore/UIButton/UIButton+Category/UIButton+UI/UIButton+UI.h
@@ -18,7 +18,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 /// ⚠️当font描绘的文字,或者文字大于UIButton的frame,UIButton的Title将不会显现⚠️
-@interface UIButton (UI)
+@interface UIButton (UI)<BaseProtocol>
 /// 为了迎合点语法而故意把下列方法属性化
 /// Common
 @property(nonatomic,strong)UIFont *titleFont;
@@ -88,3 +88,33 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
+/**
+ 关于UIButtonConfiguration 和 configurationUpdateHandler 的使用
+ 
+ #import <UIKit/UIKit.h>
+
+ // 创建一个 UIButtonConfiguration
+ UIButtonConfiguration *buttonConfig = [UIButtonConfiguration plainButtonConfiguration];
+ buttonConfig.title = @"按钮标题";
+ buttonConfig.baseBackgroundColor = [UIColor blueColor];
+ buttonConfig.contentInsets = NSDirectionalEdgeInsetsMake(10, 20, 10, 20);
+
+ UIImage *normalImage = [UIImage imageNamed:@"normal_image"];
+ buttonConfig.image = normalImage;
+
+ // 创建一个按钮并应用配置
+ UIButton *button = [UIButton buttonWithConfiguration:buttonConfig primaryAction:nil];
+
+ // 在按钮高亮状态变化时，使用 configurationUpdateHandler 来自定义图像样式
+ button.configurationUpdateHandler = ^(UIButton * _Nonnull updatedButton) {
+     if (updatedButton.isHighlighted) {
+         // 按钮处于高亮状态，切换到高亮状态的图像样式
+         UIImage *highlightedImage = [UIImage imageNamed:@"highlighted_image"];
+         updatedButton.configuration.image = highlightedImage;
+     } else {
+         // 按钮处于正常状态，切换到正常状态的图像样式
+         UIImage *normalImage = [UIImage imageNamed:@"normal_image"];
+         updatedButton.configuration.image = normalImage;
+     }
+ };
+ */
